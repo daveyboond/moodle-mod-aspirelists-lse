9be83813c9f1ed1506cffedf1e2feccd386113ac
Steve Bond <s.bond1@lse.ac.uk>
2013-07-19 16:38:24 +0100

    Fix capabilities

--- db/access.php
+++ db/access.php
@@ -6,8 +6,21 @@
  */
 $capabilities = array(
 
+    'mod/aspirelists:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+    
     // Ability to manage/publish reading lists
-    'mod/talis:pub' => array(
+    'mod/aspirelists:pub' => array(
         'riskbitmask' => RISK_MANAGETRUST & RISK_CONFIG,
         'captype' => 'write',
         'contextlevel' => CONTEXT_MODULE,
